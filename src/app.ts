36f538403b512eebb65138245e3d3c151737a87a
Richard Kasperowski <r@kasperowski.com>
2024-09-27 14:35:23 -0400

    Alternate reality - back to basics to prove we can deploy the Deno app

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,38 @@
-import { serveFile } from "https://deno.land/std@0.204.0/http/file_server.ts";
+// import { serveFile } from "https://deno.land/std@0.204.0/http/file_server.ts";
+//
+// // Slightly complicated so we can mock createHandler() in unit tests
+//
+// export type ServeFileWrapper = (path: string, req: Request) => Promise<Response>;
+//
+// export const defaultServeFileWrapper: ServeFileWrapper = async (path: string, req: Request): Promise<Response> => {
+//     try {
+//         return await serveFile(req, path);
+//     } catch {
+//         return new Response("404 Not Found", { status: 404 });
+//     }
+// };
+//
+// export function createHandler(serveFileWrapperFn: ServeFileWrapper = defaultServeFileWrapper) {
+//     return async function handler(req: Request): Promise<Response> {
+//         const url = new URL(req.url);
+//         let filepath = decodeURIComponent(url.pathname);
+//
+//         if (filepath === "" || filepath === "/") {
+//             filepath = "/index.html";
+//         }
+//
+//         return await serveFileWrapperFn(`../public${filepath}`, req);
+//     };
+// }
+//
+// // Only start the server if this file is run directly
+// if (import.meta.main) {
+//     const handler = createHandler();
+//     Deno.serve({ port: 8000 }, handler);
+// }
 
-// Slightly complicated so we can mock createHandler() in unit tests
-
-export type ServeFileWrapper = (path: string, req: Request) => Promise<Response>;
-
-export const defaultServeFileWrapper: ServeFileWrapper = async (path: string, req: Request): Promise<Response> => {
-    try {
-        return await serveFile(req, path);
-    } catch {
-        return new Response("404 Not Found", { status: 404 });
-    }
-};
-
-export function createHandler(serveFileWrapperFn: ServeFileWrapper = defaultServeFileWrapper) {
-    return async function handler(req: Request): Promise<Response> {
-        const url = new URL(req.url);
-        let filepath = decodeURIComponent(url.pathname);
-
-        if (filepath === "" || filepath === "/") {
-            filepath = "/index.html";
-        }
-
-        return await serveFileWrapperFn(`../public${filepath}`, req);
-    };
+function handler(_req: Request): Response {
+    return new Response("Hello World from Deno");
 }
 
-// Only start the server if this file is run directly
-if (import.meta.main) {
-    const handler = createHandler();
-    Deno.serve({ port: 8000 }, handler);
-}
+Deno.serve({ port: 8000 }, handler);
